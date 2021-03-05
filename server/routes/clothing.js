6aeed4ab57b8d958a0491f6dbd211512e06d4570
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-03-05 15:02:16 -0600

    feat(async-patterns-in-node): module 2

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const fs = require('fs');
-const datafile = 'server/data/clothing.json';
+const express = require("express");
+const fs = require("fs");
+const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+router.route("/").get(function (req, res) {
+  getClothingData((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("returning clothing data");
+      res.send(data);
+    }
   });
+
+  console.log("doing more work");
+});
+
+function getClothingData(callback) {
+  fs.readFile(datafile, "utf8", (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let clothingData = JSON.parse(data);
+      callback(null, clothingData);
+    }
+  });
+}
 
 module.exports = router;
